db86e8ab8662a5a23ff2590d1eca11d46528199f
artursantana <artursantana843@gmail.com>
2023-12-15 23:55:25 +0000

    first commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
             <div className="about_data grid">
                 <div className="about_info">
                     <p className="about_description">
-                        Web Developer from Portugal, I have many projects with a lot tools how REACT.JS and TYPESCRIPT
+                        As a Front End Developer , I have many projects with a lot tools how REACT.JS and TYPESCRIPT
                     </p>
                     <a href="#" className='btn'> Download CV</a>
                 </div>
